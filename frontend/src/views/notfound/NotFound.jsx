944701b92d9963abf866bbf96343ec942d4c7c77
Leonardo Duque <leoartdev@gmail.com>
2024-04-24 21:18:13 -0400

    commit ingresando a git

--- frontend/src/views/notfound/NotFound.jsx
+++ frontend/src/views/notfound/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NOT FOUND 404 404</div>;
+  return (
+    <div>
+      NOT FOUND 404 404 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Enim tenetur amet eum voluptas dolore? Suscipit?
+    </div>
+  );
 };
 
 export default NotFound;
