9f399603a288d7d0d87ae6c3204afe9cb86bd0c2
Leonardo Duque <leoartdev@gmail.com>
2024-05-06 15:53:31 -0400

    Resolución de conflictos en archivos frontend/src/components/pooldetails/PoolDetails.jsx, frontend/src/views/home/Home.jsx y frontend/src/views/login/LogIn.jsx

---
+++ WEB/src/views/home/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
+import Banner from "../../components/banner/Banner";
+import PoolCard from "../../components/poolcard/PoolCard";
+import Carrousel from "../../components/Carrousel/Carrousel";
+import PoolDetails from "../../components/pooldetails/PoolDetails";
+import LogIn from "../../views/login/LogIn";
+import Register from "../../components/register/Register";
+import Profile from "../../components/profile/Profile";
+// import Pools from '../../components/pools/Pools'
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Banner /> */
+      <Pools />
+      <Carrousel />
+      <PoolCard />
+      <PoolDetails />
+      <LogIn />
+      <Register />
+      <Profile />
+    </>
+  );
+};
+
+export default Home;
