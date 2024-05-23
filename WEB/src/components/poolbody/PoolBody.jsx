5ef4c533030c62e51480509c61d92169e8bca4ad
Leo Duque <134341762+leodev182@users.noreply.github.com>
2024-05-22 22:33:13 -0400

    Delete WEB directory

--- WEB/src/components/poolbody/PoolBody.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PoolBody = () => {
-  return (
-    <div>PoolBody</div>
-  )
-}
-
-export default PoolBody
--- WEB/src/views/profile/Profile.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ProfileBox from "../../components/profile/ProfileBox"
-
-const Profile = () => {
-  return <ProfileBox/>;
-};
-
-export default Profile;
